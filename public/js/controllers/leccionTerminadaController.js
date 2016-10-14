8c1d809d084b1d8789cbe8b80c71464c5f9f3cb9
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2016-10-13 21:06:36 -0500

    Fix. Parejas con imagenes. Fix Lección finalizada

--- public/js/controllers/leccionTerminadaController.js
+++ public/js/controllers/leccionTerminadaController.js
@@ -1,5 +1,5 @@
 /*global angular*/
-nutrifamiApp.controller('LeccionTerminadaController', function ($scope, $anchorScroll, bsLoadingOverlayService) {
+nutrifamiApp.controller('LeccionTerminadaController', function($scope, $anchorScroll, $routeParams, bsLoadingOverlayService) {
     'use strict';
 
     $anchorScroll();
@@ -7,13 +7,45 @@ nutrifamiApp.controller('LeccionTerminadaController', function ($scope, $anchorS
     /* Overloading*/
     bsLoadingOverlayService.start();
     /* Se apaga cuando el todo el contenido de la vista ha sido cargado*/
-    $scope.$on('$viewContentLoaded', function () {
-        /* Se le agrega 0,3 segundos para poder verlo ver inicialmente
-         * cuando el contenido se demore mucho en cargar se puede quitar el timeout*/
+    $scope.$on('$viewContentLoaded', function() {
         bsLoadingOverlayService.stop();
     });
 
     $scope.mensaje = "Ha finalizado la leccion";
 
+    $scope.leccion = nutrifami.training.getLeccion($routeParams.leccion);
+    $scope.audios = {
+        'leccionCompletada': 'audios/muy-bien-leccion-completada.mp3',
+        'audioPuntos': 'audios/' + $scope.leccion.finalizado.puntos + '-puntos-ganados.mp3',
+        'audioFinalizado': 'assets/' + $scope.leccion.finalizado.audio.nombre,
+    };
+
+
+    //AudioService.preloadSimple($scope.audios);
+
+
+    /*
+        $timeout(function() {
+            AudioService.play('leccionCompletada');
+        }, 1000);*/
+
+    $scope.leccionCompletada = {};
+    //$scope.leccionCompletada.audio = ngAudio.load("audios/muy-bien-leccion-completada.mp3");
+
+
+
+    console.log($scope.leccion);
+
+    //$scope.leccion.finalizado.audio.audio = ngAudio.load("assets/" + $scope.leccion.finalizado.audio.nombre);
+    //$scope.leccion.finalizado.audio.audioPuntos = ngAudio.load("audios/" + $scope.leccion.finalizado.puntos + "-puntos-ganados.mp3");
+
+    $scope.playAudio = function(audio) {
+        AudioService.play(audio);
+    };
+
+    $scope.continuar = function() {
+        AudioService.unload($scope.audios);
+        $location.path("/app/capacitacion/" + $stateParams.modulo);
+    };
+
 });
-        
