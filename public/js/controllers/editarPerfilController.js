bdd7691fdbc7e1d90441ac6771dca47aabb16d64
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2017-07-03 20:20:44 -0500

    -[ADD] Editar país, departamento y municipio.

--- public/js/controllers/editarPerfilController.js
+++ public/js/controllers/editarPerfilController.js
@@ -1,5 +1,5 @@
 /*global angular*/
-nutrifamiApp.controller('EditarPerfilController', function($scope, $location, PerfilService, $anchorScroll, bsLoadingOverlayService, $timeout, UsuarioService) {
+nutrifamiApp.controller('EditarPerfilController', function($scope, $location, $filter, PerfilService, $anchorScroll, bsLoadingOverlayService, $timeout, UsuarioService) {
     'use strict';
 
     $anchorScroll();
@@ -9,11 +9,13 @@ nutrifamiApp.controller('EditarPerfilController', function($scope, $location, Pe
         bsLoadingOverlayService.stop();
     });
 
-    $scope.usuarioActivo = UsuarioService.getUsuarioActivo();
-    PerfilService.getLocation(function(response) {
-        console.log(response);
 
-    });
+
+
+
+    $scope.usuarioActivo = UsuarioService.getUsuarioActivo();
+
+
     $scope.usuarioActivo.generos = {
         availableOptions: [
             { id: 'F', name: 'Femenino' },
@@ -29,8 +31,64 @@ nutrifamiApp.controller('EditarPerfilController', function($scope, $location, Pe
             { id: 'OTROS', name: 'Otros' },
             { id: 'NINGUNO', name: 'Ninguno' }
         ],
-        selectedOption: { id: $scope.usuarioActivo.etnia, name: usuarioActivo.etnia }
+        selectedOption: { id: $scope.usuarioActivo.etnia, name: $scope.usuarioActivo.etnia }
     };
+
+    PerfilService.getLocation(function(response) {
+
+        $scope.paises = response.paises;
+
+        $scope.departamentos = response.estados;
+        $scope.ciudades = response.ciudades;
+
+        for (var pais in response.paises) {
+            if ($scope.usuarioActivo.pais == response.paises[pais].name) {
+                $scope.usuarioActivo.pais_id = {
+                    id: response.paises[pais].id,
+                    name: response.paises[pais].name,
+                    phonecode: response.paises[pais].phonecode,
+                    sortname: response.paises[pais].sortname,
+                }
+
+            }
+        }
+
+        for (var departamento in response.estados) {
+            if ($scope.usuarioActivo.departamento == response.estados[departamento].name) {
+                console.log(response.estados[departamento])
+                $scope.usuarioActivo.departamento_id = {
+                    id: response.estados[departamento].id,
+                    name: response.estados[departamento].name,
+                    country_id: response.estados[departamento].country_id,
+                };
+                $scope.departamentos_filter = [$scope.usuarioActivo.departamento_id];
+
+            }
+        }
+
+        for (var ciudad in response.ciudades) {
+            if ($scope.usuarioActivo.municipio == response.ciudades[ciudad].name) {
+                console.log(response.ciudades[ciudad]);
+                $scope.usuarioActivo.municipio_id = {
+                    id: response.ciudades[ciudad].id,
+                    name: response.ciudades[ciudad].name,
+                    state_id: response.ciudades[ciudad].state_id,
+                };
+                $scope.ciudades_filter = [$scope.usuarioActivo.municipio_id];
+            }
+        }
+
+
+
+
+
+
+
+    });
+
+
+
+
 
 
     if ($scope.usuarioActivo.birthdate !== null) {
@@ -46,11 +104,37 @@ nutrifamiApp.controller('EditarPerfilController', function($scope, $location, Pe
 
 
     $scope.update = function() {
+
         bsLoadingOverlayService.start();
 
         var usuarioActivo = Object.assign({}, $scope.usuarioActivo);
+        console.log(usuarioActivo);
         usuarioActivo.genero = $scope.usuarioActivo.generos.selectedOption.id || '';
         usuarioActivo.etnia = $scope.usuarioActivo.etnias.selectedOption.id || '';
+
+
+        for (var pais in $scope.paises) {
+            if ($scope.paises[pais].id == usuarioActivo.pais_id) {
+                usuarioActivo.pais = $scope.paises[pais].name;
+
+            }
+        }
+
+        for (var departamento in $scope.departamentos) {
+            if ($scope.departamentos[departamento].id == usuarioActivo.departamento_id) {
+                usuarioActivo.departamento = $scope.departamentos[departamento].name;
+
+            }
+        }
+
+        for (var ciudad in $scope.ciudades) {
+            if ($scope.ciudades[ciudad].id == usuarioActivo.municipio_id) {
+                usuarioActivo.municipio = $scope.ciudades[ciudad].name;
+
+            }
+        }
+
+
         var tempMonth = $scope.usuarioActivo.nacimiento.getMonth() + 1;
         var tempDay = $scope.usuarioActivo.nacimiento.getDate();
         if (tempMonth < 10) {
@@ -60,6 +144,14 @@ nutrifamiApp.controller('EditarPerfilController', function($scope, $location, Pe
         delete usuarioActivo["generos"];
         delete usuarioActivo["etnias"];
         delete usuarioActivo["nacimiento"];
+        delete usuarioActivo["pais_id"];
+        delete usuarioActivo["departamento_id"];
+        delete usuarioActivo["municipio_id"];
+
+
+
+
+
 
         PerfilService.editarUsuario(usuarioActivo, function(response) {
             if (response.success) {
@@ -92,6 +184,17 @@ nutrifamiApp.controller('EditarPerfilController', function($scope, $location, Pe
         });
 
     };
+
+    $scope.updateDropDownDepartamentos = function(pais) {
+        console.log(pais)
+        $scope.departamentos_filter = $filter('filter')($scope.departamentos, { country_id: pais });
+    }
+
+    $scope.updateDropDownCiudades = function(estado) {
+        console.log(estado);
+        $scope.ciudades_filter = $filter('filter')($scope.ciudades, { state_id: estado });
+        console.log($scope.ciudades_filter);
+    }
 });
 
 nutrifamiApp.filter('capitalize', function() {
