1353fa7cababacf23346267426b3d395fff49f6d
Cloud User <ec2-user@ip-172-31-8-16.ec2.internal>
2018-02-17 13:12:08 -0500

    Fix. archivo json.php

--- public/js/json.php
+++ public/js/json.php
@@ -3,4 +3,8 @@ $file = $_GET["file"];
 $data = file_get_contents($file);
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 echo $data;
